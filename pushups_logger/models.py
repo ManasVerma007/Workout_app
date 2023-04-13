ab3cf9de7056e0eed1c731318038a57165afb4c1
Manas <manasverma012345@gmail.com>
2023-04-13 11:55:22 +0530

    okay 1.0

--- pushups_logger/models.py
+++ pushups_logger/models.py
@@ -8,3 +8,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    workouts = db.relationship('Workout', backref='author', lazy=True)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pushups = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    comment = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
